a584af0b8e9413bd1b065f030cc465a0ea95d0bc
KamilKalandyk <k.kalandyk@gmail.com>
2015-01-18 11:15:12 +0100

    initial UI setup

--- iOSDebtControl/ViewController.swift
+++ iOSDebtControl/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cancelAddingDebt(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
