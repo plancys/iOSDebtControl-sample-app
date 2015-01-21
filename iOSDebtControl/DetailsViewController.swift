c237a690efc5f02b37202cca9742ab14cf801bf6
KamilKalandyk <k.kalandyk@gmail.com>
2015-01-21 20:45:00 +0100

    sending sms

--- iOSDebtControl/DetailsViewController.swift
+++ iOSDebtControl/DetailsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
     var debtDesc: String = ""
     
@@ -25,6 +26,22 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     @IBAction func requestRepayment(sender: AnyObject) {
+        if MFMessageComposeViewController.canSendText() {
+            let messageVC = MFMessageComposeViewController()
+            messageVC.messageComposeDelegate = self
+            messageVC.recipients = [debt!.personPhoneNumber]
+            messageVC.body = "Hey \(debt?.connectedPerson)! Could you repay me debt (\(debt?.description)) for \(debt?.amount) USD?"
+            self.presentViewController(messageVC, animated: true, completion: nil)
+        }
+        else {
+            println("User hasn't setup Messages.app")
+            let alertController = UIAlertController(title: "Unable to send SMS", message:
+                "You don't have SMS application. Remember, you cannot test sms functionality on emulator!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -40,8 +57,8 @@ class DetailsViewController: UIViewController {
         areYouDebtorLabel.text = areYouDebtor
         
         var dateFormatter: NSDateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "MM-dd-yyyy"
-        dateFormatter.timeZone = NSTimeZone(abbreviation: "UTC")
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        dateFormatter.timeZone = NSTimeZone(abbreviation: "GMT")
         let date :NSDate = debt!.creationDate
         dateLabel.text =  dateFormatter.stringFromDate(date)
         
@@ -55,14 +72,20 @@ class DetailsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        switch (result.value) {
+        case MessageComposeResultCancelled.value:
+            println("Message was cancelled")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultFailed.value:
+            println("Message failed")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultSent.value:
+            println("Message was sent")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        default:
+            break;
+        }
     }
-    */
 
 }
